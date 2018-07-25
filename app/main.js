7205cee34579035459d4655d02229e31f0abdba4
sunchengzhou013 <sunchengzhou013@lu.com>
2018-07-25 18:55:06 +0800

    add file1

--- app/main.js
+++ app/main.js
@@ -1,4 +1,21 @@
 import React from "react";
 import {render} from "react-dom";
+import Home from './home/home.jsx';
+import { createStore } from 'redux';
+import counter from './reducers';
+
+const store = createStore(counter);
+
 import "./main.css";
-render(<CommentBox />,document.getElementById('root'));
+var renderHtml = () => {render(<Home 
+    store={store}
+    value={store.getState().toString()}
+    onadd = {() => {store.dispatch({
+        type: 'ADD'
+    })}}
+    onreduce = {() => {store.dispatch({
+        type: 'REDUCE'
+    })}}
+    />,document.getElementById('root'))};
+renderHtml();
+store.subscribe(renderHtml);
---
+++ app/reducers/index.js
@@ -0,0 +1,7 @@
+export default (state= 0, action) => {
+  switch(action.type) {
+    case 'ADD': return state + 1;
+    case 'REDUCE': return state -1;
+    default : return state;
+  }
+}
